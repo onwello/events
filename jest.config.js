5eafca70277fcaad56a88b7b2e29e25048c8938c
Onwello Team <ghoucine@gmail.com>
2025-08-20 20:38:09 +0300

    ci: update Jest config to exclude Redis integration tests
    - Add testPathIgnorePatterns to jest.config.js to exclude Redis integration tests
    - Update package.json scripts for better test organization
    - Add test:redis script for running Redis tests when needed
    - Unit tests now run without Redis dependencies

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.spec.ts'],
+  testPathIgnorePatterns: [
+    'redis-integration-tests.spec.ts',
+    'redis-publishing.spec.ts', 
+    'redis-basic-test.spec.ts'
+  ],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
